477f61934dd1c63e9bd87d50bb22fd61e970f441
emmanueliyanu21 <emmanueliyanu2012@gmail.com>
2023-01-20 04:26:30 +0100

    some fixes

--- src/components/Table.js
+++ src/components/Table.js
@@ -2,6 +2,15 @@ import React from "react";
 import Download from "./../assets/icons/download-cloud.png";
 import { Icon } from '@iconify/react';
 
+import UserImage from '../assets/avatar/avatargroup.png'
+import UserImage1 from '../assets/avatar/avatargroup1.png'
+import UserImage2 from '../assets/avatar/avatargroup2.png'
+import UserImage3 from '../assets/avatar/avatargroup3.png'
+import UserImage4 from '../assets/avatar/avatargroup4.png'
+import UserImage5 from '../assets/avatar/avatargroup5.png'
+import UserImage6 from '../assets/avatar/avatargroup6.png'
+import UserImage7 from '../assets/avatar/avatargroup3.png'
+
 function Table() {
   const data = [
     {
@@ -10,7 +19,7 @@ function Table() {
       amount: 10,
       date: "Dec 1, 2022",
       status: "paid",
-      users: './../assets/avatar/avatargroup.png'
+      users: UserImage
     },
     {
       id: 2,
@@ -18,7 +27,7 @@ function Table() {
       amount: 10,
       date: "Nov 1, 2022",
       status: "paid",
-      users: './../assets/avatar/avatargroup1.png'
+      users: UserImage1
     },
     {
       id: 3,
@@ -26,7 +35,7 @@ function Table() {
       amount: 10,
       date: "Oct 1, 2022",
       status: "paid",
-      users: './../assets/avatar/avatargroup2.png'
+      users: UserImage2
     },
     {
       id: 4,
@@ -34,7 +43,7 @@ function Table() {
       amount: 10,
       date: "Sept 1, 2022",
       status: "paid",
-      users: './../assets/avatar/avatargroup3.png'
+      users: UserImage3
     },
     {
       id: 5,
@@ -42,7 +51,7 @@ function Table() {
       amount: 10,
       date: "Aug 1, 2022",
       status: "paid",
-      users: './../assets/avatar/avatargroup4.png'
+      users: UserImage4
     },
     {
       id: 6,
@@ -50,7 +59,7 @@ function Table() {
       amount: 10,
       date: "Jul 1, 2022",
       status: "paid",
-      users: './../assets/avatar/avatargroup5.png'
+      users: UserImage5
     },
     {
       id: 7,
@@ -58,7 +67,7 @@ function Table() {
       amount: 10,
       date: "Jun 1, 2022",
       status: "paid",
-      users: './../assets/avatar/avatargroup6.png'
+      users: UserImage6
     },
     {
       id: 8,
@@ -66,23 +75,23 @@ function Table() {
       amount: 10,
       date: "May 1, 2022",
       status: "paid",
-      users: './../assets/avatar/avatargroup7.png'
+      users: UserImage7
     },
   ];
 
   return (
-    <div>
-      <div className="max-w-6xl mx-auto">
-        <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <div >
+      <div className="max-w-6xl mx-auto ">
+        <div className="relative overflow-x-auto border border-[#EAECF0] shadow-3xl sm:rounded-lg">
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 transparent dark:text-gray-400">
+            <thead className="text-xs text-deep-grey font-inter border-b border-[#EAECF0] font-medium bg-gray-50 transparent dark:text-gray-400">
               <tr>
                 <th scope="col" className="p-4">
                   <div className="flex items-center">
                     <input
                       id="checkbox-all-search"
                       type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                      className="w-4 h-4 hover:cursor-pointer h-4 hover:border-purple-color hover:bg-light-purple-color text-purple-color bg-gray-100 border-gray-300 rounded focus:ring-0 dark:focus:ring-0 dark:ring-offset-gray-800"
                     />
                     <label htmlFor="checkbox-all-search" className="sr-only">
                       checkbox
@@ -102,7 +111,7 @@ function Table() {
                   Status
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  User on plan
+                  Users on plan
                 </th>
                 <th scope="col" className="px-6 py-3">
                   <span className="sr-only"></span>
@@ -113,7 +122,7 @@ function Table() {
               data.map((item, index) => (
                 <tbody key={index}>
                   <tr
-                    className="bg-white border-b border-[#EAECF0] white hover:bg-gray-50 dark:hover:bg-bgcolor"
+                    className="bg-white hover:cursor-pointer border-b border-[#EAECF0] text-sm white hover:bg-gray-50 dark:hover:bg-bgcolor"
                     
                   >
                     <td className="w-4 p-4">
@@ -121,7 +130,7 @@ function Table() {
                         <input
                           id="checkbox-table-search-1"
                           type="checkbox"
-                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                          className="w-4 hover:cursor-pointer h-4 hover:border-purple-color hover:bg-light-purple-color text-purple-color bg-gray-100 border-gray-300 rounded focus:ring-0 dark:focus:ring-0 dark:ring-offset-gray-800  dark:bg-gray-700 dark:border-gray-600"
                         />
                         <label htmlFor="checkbox-table-search-1" className="sr-only">
                           checkbox
@@ -130,12 +139,12 @@ function Table() {
                     </td>
                     <th
                       scope="row"
-                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+                      className="px-6 py-4 font-medium text-sm text-text-color whitespace-nowrap"
                     >
                       {item.productName}
                     </th>
-                    <td className="px-6 py-4">USD ${item.amount}.00</td>
-                    <td className="px-6 py-4">{item.date}</td>
+                    <td className="px-6 py-4 text-deep-grey font-inter font-medium">USD ${item.amount}.00</td>
+                    <td className="px-6 py-4 text-deep-grey font-inter font-medium">{item.date}</td>
                     <td className="px-6 py-4 green ">
                       <span className="bg-[#ECFDF3] flex items-center w-20 h-7 font-medium font-inter p-2 text-[#027A48] rounded-full">
                       <Icon icon="material-symbols:check-small-rounded" width="24" height="24" />
@@ -145,7 +154,7 @@ function Table() {
                       <img src={item.users} alt="" />
                       </td>
                     <td className="px-6 py-4 text-right">
-                      <img src={Download} alt="" />
+                      <img width={16} height={16} src={Download} alt="" />
                     </td>
                   </tr>
                 </tbody>
--- src/modules/Settings/Settings.js
+++ src/modules/Settings/Settings.js
@@ -1,36 +1,39 @@
 import Table from "../../components/Table";
 
-import Mail from "../../mail.png";
-import Plus from "../../assets/plus.png";
 import Visa from "../../assets/visa-logo.png";
+
 import Mastercard from "../../assets/Mastercard.png";
 
+import Download from "./../../assets/icons/download-cloud.png";
+
+import { Icon } from "@iconify/react";
+
 const Settings = () => {
-    const data = [
-        "My details",
-        "Profile",
-        "Password",
-        "Team",
-        "Plan",
-        "Billing",
-        "Notification",
-        "Integration",
-        "API",
-      ];
+  const data = [
+    "My details",
+    "Profile",
+    "Password",
+    "Team",
+    "Plan",
+    "Billing",
+    "Notification",
+    "Integration",
+    "API",
+  ];
   return (
     <>
-        <div className="wrapper bgcolor bg-white pt-8 pb-12 px-8 bg-light-gray">
+      <div className="wrapper bgcolor bg-white pt-8 pb-12 px-8 bg-light-gray">
         <div className="header">
-          <h2 className="text-color font-inter font-medium text-3xl pb-1 not-italic">
+          <h2 className="text-text-color font-inter font-medium text-3xl pb-1 not-italic">
             Settings
           </h2>
-          <p className="grey-text font-inter not-italic pb-5 text-base font-normal">
+          <p className="text-grey-text font-inter not-italic pb-5 text-base font-normal">
             Manage your team and preferences here.
           </p>
         </div>
         <div className="overflow-x-auto this">
           <div className="nav max-w-5xl mt-2.5 mb-10">
-            <div className="w-full">
+            <div className="w-[1000px]">
               {data.map((item, i) => (
                 <span
                   key={i}
@@ -43,19 +46,19 @@ const Settings = () => {
           </div>
         </div>
         <div className="body-content border-b border-[#EAECF0] mb-6 pb-5">
-          <h2 className="text-color font-inter pb-1.5 leading-7 font-medium not-italic">
+          <h2 className="text-text-color font-inter pb-1 leading-7 font-medium not-italic">
             Payment method
           </h2>
-          <p className="grey-text font-inter leading-5 text-sm font-normal not-italic">
+          <p className="text-grey-text font-inter leading-5 text-sm font-normal not-italic">
             Update your billing details and address.
           </p>
         </div>
         <div className="grid grid-cols-[30%_70%] body-content2 border-b border-[#EAECF0] gap-5 grid pb-5 mb-5">
           <div>
-            <h2 className="header-text leading-5leading-5 text-sm not-italic">
+            <h2 className="text-header-text font-inter leading-5 text-sm not-italic">
               Contact email
             </h2>
-            <p className="grey-text leading-5 text-sm not-italic">
+            <p className="text-grey-text font-inter leading-5 text-sm not-italic">
               Where should invoices be sent?
             </p>
           </div>
@@ -64,15 +67,15 @@ const Settings = () => {
               <div className="pr-3">
                 <input
                   type="checkbox"
-                  className="-mt-1 w-3.5 h-3.5 hover:border-purple-color hover:bg-light-purple-color hover:cursor-pointer rounded-full focus:ring-0"
+                  className="-mt-2 w-3.5 h-3.5 hover:border-purple-color hover:bg-light-purple-color hover:cursor-pointer rounded-full focus:ring-0"
                 />
               </div>
               <div>
                 <div className="firstN1 pb-5">
-                  <h2 className="leading-5leading-5 text-sm not-italic">
+                  <h2 className="text-header-text font-inter font-medium leading-5 leading-5 text-sm not-italic">
                     Send to my account email
                   </h2>
-                  <p className="leading-5 text-sm font-normal not-italic">
+                  <p className="leading-5 text-dark-grey font-inter text-sm font-normal not-italic">
                     olivia@untitledui.com
                   </p>
                 </div>
@@ -82,13 +85,20 @@ const Settings = () => {
               <div className="pr-3">
                 <input
                   type="checkbox"
-                  className="-mt-1 w-3.5 hover:border-purple-color hover:bg-light-purple-color hover:cursor-pointer h-3.5 rounded-full focus:ring-0"
+                  className="-mt-2 w-3.5 hover:border-purple-color hover:bg-light-purple-color hover:cursor-pointer h-3.5 rounded-full focus:ring-0"
                 />
               </div>
               <div>
-                <h2>Send to an alternative email</h2>
-                <div className="shadow-3xl border border-[#d0d5dd] white items-center rounded-lg w-96 max-w-lg mt-2.5 flex bg-white pt-2.5 pb-3 pr-5 pl-3">
-                  <img className="w-7 h-5 pr-2" src={Mail} alt=""></img>
+                <h2 className="text-header-text text-sm font-inter font-medium not-italic">
+                  Send to an alternative email
+                </h2>
+                <div className="mail-details shadow-3xl border border-[#d0d5dd] white items-center rounded-lg w-[450px] max-w-lg mt-2.5 flex bg-white pt-2 pb-2 pr-5 pl-3">
+                  <Icon
+                    icon="ic:round-mail-outline"
+                    className="pr-2 text-[#667085]"
+                    width="28"
+                    height="28"
+                  />
                   <span className="text-color font-inter leading-6 text-base font-normal not-italic">
                     billing@untitledui.com
                   </span>
@@ -97,17 +107,17 @@ const Settings = () => {
             </div>
           </div>
         </div>
-        <div className="grid grid-cols-[30%_70%] body-content2 border-b border-[#EAECF0] gap-5 grid pb-5 mb-5">
+        <div className="grid grid-cols-[30%_70%] body-content2  gap-5 grid pb-5 mb-5">
           <div>
-            <h2 className="header-text font-inter leading-5 text-lg font-medium">
+            <h2 className="text-header-text font-inter leading-5 text-sm font-medium">
               Card details
             </h2>
-            <p className="font-inter leading-5 font-normal">
+            <p className="font-inter leading-5 text-dark-grey text-sm font-normal">
               Select default payment method.
             </p>
           </div>
           <div>
-            <div className="card border border-[#eaecf0] max-w-screen-md gap-5 bg-white rounded-lg flex mb-2.5 bg-white p-5">
+            <div className="card border hover:cursor-pointer hover:border-[purple] hover:border-dotted border-[#eaecf0] max-w-screen-md gap-5 bg-white rounded-lg flex mb-2.5 bg-white p-4">
               <div>
                 <img
                   className="border border-[#eaecf0] bg-white rounded p-2.5 bg-white"
@@ -116,18 +126,23 @@ const Settings = () => {
                 ></img>
               </div>
               <div>
-                <h2 className="dark-text-grey font-inter pb-1 leading-5 text-sm font-medium not-italic">
+                <h2 className="text-dark-text-grey font-inter pb-0 leading-5 text-sm font-medium not-italic">
                   Visa ending in 1234
                 </h2>
-                <p className="grey-text font-inter pb-2.5 text-sm not-italic">
+                <p className="text-grey-text font-inter pb-1.5 text-sm not-italic">
                   Expiry 06/2024
                 </p>
                 <p>
-                  Set as default <span>Edit</span>
+                  <span className="text-deep-grey pr-1 text-sm font-medium not-italic font-inter">
+                    Set as default
+                  </span>{" "}
+                  <span className="text-sm text-[purple] font-medium font-inter not-italic">
+                    Edit
+                  </span>
                 </p>
               </div>
             </div>
-            <div className="card border border-[#eaecf0] max-w-screen-md gap-5 bg-white rounded-lg flex bg-white p-5">
+            <div className="card border hover:cursor-pointer hover:border-[purple] hover:border-dotted border-[#eaecf0] max-w-screen-md gap-5 bg-white rounded-lg flex bg-white p-4">
               <div>
                 <img
                   className="border border-[#eaecf0] bg-white rounded p-2.5 bg-white"
@@ -136,30 +151,51 @@ const Settings = () => {
                 ></img>
               </div>
               <div>
-                <h2 className="dark-text-grey pb-1 font-inter leading-5 text-sm font-medium not-italic">
+                <h2 className="text-dark-text-grey pb-0 font-inter leading-5 text-sm font-medium not-italic">
                   Visa ending in 1234
                 </h2>
-                <p className="pb-2.5 font-normal text-sm not-italic">
+                <p className="text-grey-text font-inter pb-1.5 font-normal text-sm not-italic">
                   Expiry 06/2024
                 </p>
-                <p>
-                  Set as default <span>Edit</span>
+                <p className="text-sm text-">
+                  <span className="text-deep-grey pr-1 text-sm font-medium not-italic font-inter">
+                    Set as default
+                  </span>{" "}
+                  <span className="text-sm text-[purple] font-medium font-inter not-italic">
+                    Edit
+                  </span>
                 </p>
               </div>
             </div>
             <div className="mt-2.5 gap-2 flex items-center">
-              <img className="h-5" src={Plus} alt=""></img>
-              Add new payment method
+              <Icon
+                icon="material-symbols:add"
+                className="pr-1 text-dark-grey"
+                width="28"
+                height="28"
+              />
+              <span className="-ml-1 text-sm font-inter not-italic  text-dark-grey">
+                Add new payment method
+              </span>
             </div>
           </div>
         </div>
         <div className="body-content">
-          <h2 className="font-medium not-italic">Billing history</h2>
+          <div className="flex justify-between mb-5 items-center">
+            <h2 className="font-medium font-inter not-italic text-lg text-text-color">
+              Billing history
+            </h2>
+            <button className="shadow-3xl border border-[#d0d5dd] px-4 py-2 rounded flex items-center gap-2">
+              <img width={16} height={16} src={Download} alt="" />
+              <span className="text-header-text font-inter font-medium text-sm not-italic">Download all</span>
+            </button>
+          </div>
+
           <Table />
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export {Settings}
+export { Settings };
